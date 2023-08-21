6d3129ec5e0fd1f3a52857316ba9e1af80f91090
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-08-20 22:21:40 -0700

    add unit tests for add(T item); .getSize(), and .T get(int index) methods

--- test/sri_assignment_7/com/coderscampus/sri/CustomArrayListTest.java
+++ test/sri_assignment_7/com/coderscampus/sri/CustomArrayListTest.java
@@ -18,16 +18,92 @@ class CustomArrayListTest {
 	@Test
 	void should_add_one_item_to_list() {
 		// Arrange:
-		CustomList<Integer> customList = new CustomArrayList<>();
+		CustomList<Integer> sut = new CustomArrayList<>();
 		
 		// Act
-		customList.add(10);
-		Integer expectedResult = customList.get(0);
-		Integer expectedSize = customList.getSize();
+		sut.add(10);
+		Integer expectedResult = sut.get(0);
+		Integer expectedSize = sut.getSize();
 		
 		// Assert: Integer is in the first index of customList
 		assertEquals(10, expectedResult);
 		assertEquals(1,  expectedSize);
 	}
+	
+	@Test
+	void should_add_11_items_to_list() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+		
+		// Act
+		for (int i=1; i < 12; i++) {
+			sut.add(10);
+		}
+		Integer expectedResult = sut.get(10);
+		Integer expectedSize = sut.getSize();
+		
+		// Assert: Integer is in the first index of customList
+		assertEquals(10, expectedResult);
+		assertEquals(11,  expectedSize);
+	}
+	
+	@Test
+	void should_add_21_items_to_list() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+		
+		// Act
+		for (int i=1; i < 22; i++) {
+			sut.add(10);
+		}
+		Integer expectedResult = sut.get(20);
+		Integer expectedSize = sut.getSize();
+		
+		// Assert: Integer is in the first index of customList
+		assertEquals(10, expectedResult);
+		assertEquals(21,  expectedSize);
+	}
+	
+	@Test
+	void should_return_true_with_add_1_item() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+		
+		// Act
+		Boolean expectedResult = sut.add(10);
+		
+		// Assert: Integer is in the first index of customList
+		assertEquals(true, expectedResult);
+	}
+	
+	@Test
+	void should_return_size_1_with_add_1_item() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+		
+		// Act
+		sut.add(10);
+		Integer expectedSize = sut.getSize();
+		
+		// Assert: Integer is in the first index of customList
+		assertEquals(1, expectedSize);
+	}
+	
+	@Test
+	void should_return_correct_value_at_index_5() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+		
+		// Act
+		Integer number = 0;
+		for (int i=1; i < 10; i++) {
+			sut.add(number++);
+		}
+		Integer expectedResult = sut.get(5);
+		
+		// Assert: 5th index should be number 5:
+		assertEquals(5, expectedResult);
+	}
+	
 
 }
