9f6364917dad527f9e98b8af8a53c98f955fec81
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-08-21 20:01:04 -0700

    add comments and organize unit test file

--- test/sri_assignment_7/com/coderscampus/sri/CustomArrayListTest.java
+++ test/sri_assignment_7/com/coderscampus/sri/CustomArrayListTest.java
@@ -17,9 +17,8 @@ class CustomArrayListTest {
 	// Assert
 
 	// First 4 check the add(T item) method:
-	
-	
-	
+
+	// Next 6: Checks for dynamic array size changes for both add() method versions:
 	@Test
 	void should_add_one_item_to_list() {
 		// Arrange:
@@ -68,70 +67,71 @@ class CustomArrayListTest {
 		assertEquals(10, expectedResult);
 		assertEquals(21, expectedSize);
 	}
-	// Repeat but for the add(index, value)
-		@Test
-		void should_add_one_item_to_list_with_index() {
-			// Arrange:
-			CustomList<Integer> sut = new CustomArrayList<>();
-			
-			// Act
-			sut.add(0, 10);
-			Integer expectedResult = sut.get(0);
-			Integer expectedSize = sut.getSize();
-			
-			// Assert: Integer is in the first index of customList
-			assertEquals(10, expectedResult);
-			assertEquals(1, expectedSize);
+
+	@Test
+	void should_add_one_item_to_list_with_index() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+
+		// Act
+		sut.add(0, 10);
+		Integer expectedResult = sut.get(0);
+		Integer expectedSize = sut.getSize();
+
+		// Assert: Integer is in the first index of customList
+		assertEquals(10, expectedResult);
+		assertEquals(1, expectedSize);
+	}
+
+	@Test
+	void should_add_11_items_to_list_with_index() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+		Integer index = 0;
+		// Act
+		for (int i = 1; i < 12; i++) {
+			sut.add(index++, 10);
 		}
-		
-		@Test
-		void should_add_11_items_to_list_with_index() {
-			// Arrange:
-			CustomList<Integer> sut = new CustomArrayList<>();
-			Integer index = 0;
-			// Act
-			for (int i = 1; i < 12; i++) {
-				sut.add(index++, 10);
-			}
-			Integer expectedResult = sut.get(10);
-			Integer expectedSize = sut.getSize();
-			
-			// Assert: Integer is in the first index of customList
-			assertEquals(10, expectedResult);
-			assertEquals(11, expectedSize);
+		Integer expectedResult = sut.get(10);
+		Integer expectedSize = sut.getSize();
+
+		// Assert: Integer is in the first index of customList
+		assertEquals(10, expectedResult);
+		assertEquals(11, expectedSize);
+	}
+
+	@Test
+	void should_add_21_items_to_list_with_index() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+		Integer index = 0;
+		// Act
+		for (int i = 1; i < 22; i++) {
+			sut.add(index++, 10);
 		}
-		
-		@Test
-		void should_add_21_items_to_list_with_index() {
-			// Arrange:
-			CustomList<Integer> sut = new CustomArrayList<>();
-			Integer index = 0;
-			// Act
-			for (int i = 1; i < 22; i++) {
-				sut.add(index++, 10);
-			}
-			Integer expectedResult = sut.get(20);
-			Integer expectedSize = sut.getSize();
-			
-			// Assert: Integer is in the first index of customList
-			assertEquals(10, expectedResult);
-			assertEquals(21, expectedSize);
-		}
-		
+		Integer expectedResult = sut.get(20);
+		Integer expectedSize = sut.getSize();
+
+		// Assert: Integer is in the first index of customList
+		assertEquals(10, expectedResult);
+		assertEquals(21, expectedSize);
+	}
+
+	// Next 5: Checks for return values for all functions(first 2 similar, delete?)
 	@Test
 	void should_return_true_with_add_1_item() {
 		// Arrange:
 		CustomList<Integer> sut = new CustomArrayList<>();
-		
+
 		// Act
 		Boolean expectedResult = sut.add(10);
 		Integer size_after_addition = sut.getSize();
-		
-		
+
 		// Assert: Integer is in the first index of customList
 		assertEquals(1, size_after_addition);
 		assertEquals(true, expectedResult);
 	}
+
 	@Test
 	void should_return_true_with_add_1_item_at_index_0() {
 		// Arrange:
@@ -140,14 +140,52 @@ class CustomArrayListTest {
 		// Act
 		Boolean expectedResult = sut.add(0, 10);
 		Integer size_after_addition = sut.getSize();
-		
 
 		// Assert: Integer is in the first index of customList
 		assertEquals(1, size_after_addition);
 		assertEquals(true, expectedResult);
 	}
+	
+	@Test
+	void should_return_actual_item_removed_from_list() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
 
-	// Next are to test if Exceptions are being triggered for the right cases:
+		// Act
+		sut.add(10);
+		Integer removed_value = sut.remove(0);
+		Integer size_after_removeal = sut.getSize();
+
+		// Assert: Item is removed only if size is reflected to that change too:
+		assertEquals(0, size_after_removeal);
+		assertEquals(10, removed_value);
+	}
+
+	@Test
+	void should_return_true_when_item_added_with_index() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+
+		// Act
+		Boolean result = sut.add(0, 10);
+
+		// Assert: Integer is in the first index of customList
+		assertEquals(true, result);
+	}
+
+	@Test
+	void should_return_true_when_item_added_without_index() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+
+		// Act
+		Boolean result = sut.add(10);
+
+		// Assert: Integer is in the first index of customList
+		assertEquals(true, result);
+	}
+
+	// Next 5: Checks if exceptions are thrown for the methods: 
 	@Test
 	void should_return_out_of_bounds_exception_for_get_index() {
 		// Arrange:
@@ -176,7 +214,6 @@ class CustomArrayListTest {
 		});
 	}
 
-	// If arr = [5] and add(int 1, T 4) -> arr = [5, 4] so no out of bounds exception:
 	@Test
 	void should_not_return_out_of_bounds_exception_if_index_added_is_at_new_end_of_list() {
 		// Arrange:
@@ -187,7 +224,7 @@ class CustomArrayListTest {
 
 		// Assert: Integer is in the first index of customList
 		assertDoesNotThrow(() -> {
-			sut.add(1,4);
+			sut.add(1, 4);
 		});
 	}
 
@@ -195,16 +232,16 @@ class CustomArrayListTest {
 	void should_return_out_of_bounds_exception_for_remove_at_index() {
 		// Arrange:
 		CustomList<Integer> sut = new CustomArrayList<>();
-		
+
 		// Act
 		sut.add(10);
 
-		
 		// Assert: Integer is in the first index of customList
 		assertThrows(IndexOutOfBoundsException.class, () -> {
 			sut.remove(3);
 		});
 	}
+
 	@Test
 	void should_return_out_of_bounds_exception_for_remove_at_negative_index() {
 		// Arrange:
@@ -219,45 +256,7 @@ class CustomArrayListTest {
 		});
 	}
 	
-	// Check Returns
-	@Test
-	void should_return_actual_item_removed_from_list() {
-		// Arrange:
-		CustomList<Integer> sut = new CustomArrayList<>();
-		
-		// Act
-		sut.add(10);
-		Integer removed_value = sut.remove(0);
-		Integer size_after_removeal = sut.getSize();
-		
-		// Assert: Item is removed only if size is reflected to that change too:
-		assertEquals(0, size_after_removeal);
-		assertEquals(10, removed_value);
-	}
-	@Test
-	void should_return_true_when_item_added_with_index() {
-		// Arrange:
-		CustomList<Integer> sut = new CustomArrayList<>();
-
-		// Act
-		Boolean result = sut.add(0,10);
-
-		// Assert: Integer is in the first index of customList
-		assertEquals(true, result);
-	}
-	@Test
-	void should_return_true_when_item_added_without_index() {
-		// Arrange:
-		CustomList<Integer> sut = new CustomArrayList<>();
-
-		// Act
-		Boolean result = sut.add(10);
-
-		// Assert: Integer is in the first index of customList
-		assertEquals(true, result);
-	}
-
-	// Check if getSize method works:
+	// Next 2: if default functions work- getSize() and get()
 	@Test
 	void should_return_size_1_with_add_1_item() {
 		// Arrange:
@@ -271,7 +270,6 @@ class CustomArrayListTest {
 		assertEquals(1, expectedSize);
 	}
 
-	// Check if get method works:
 	@Test
 	void should_return_correct_value_at_index_5() {
 		// Arrange:
@@ -287,8 +285,8 @@ class CustomArrayListTest {
 		// Assert: 5th index should be number 5:
 		assertEquals(5, expectedResult);
 	}
-	
-	// Tests if length was increased by 2:
+
+	// Next 3: Checks if arrays are adjusting to right sizes dynamically for add/remove:
 	@Test
 	void should_increase_array_list_size_by_two() {
 		// Arrange:
@@ -307,17 +305,18 @@ class CustomArrayListTest {
 
 		printArray(sut, "\nAdded 2 items");
 		Integer initial_size = sut.getItemArrayLength();
-		
+
 		sut.add(indexMessagePrint(ind));
 		ind++;
 		printArray(sut, "\nAdded 1 items -> Array * 2");
 		Integer final_size = sut.getItemArrayLength();
-		
+
 		Integer expectedResult = final_size / initial_size;
-		
+
 		// Assert: 5th index should be number 5:
 		assertEquals(2, expectedResult);
 	}
+
 	@Test
 	void should_increase_array_list_size_by_two_for_add_with_index() {
 		// Arrange:
@@ -325,28 +324,29 @@ class CustomArrayListTest {
 		Integer amount = 8;
 		CustomArrayList<String> sut = new CustomArrayList<String>();
 		for (ind = 1; ind <= amount; ind++) {
-			sut.add(ind-1, indexMessagePrint(ind));
+			sut.add(ind - 1, indexMessagePrint(ind));
 		}
 		printArray(sut, "Initial");
 
-		sut.add(ind-1, indexMessagePrint(ind));
+		sut.add(ind - 1, indexMessagePrint(ind));
 		ind++;
-		sut.add(ind-1, indexMessagePrint(ind));
+		sut.add(ind - 1, indexMessagePrint(ind));
 		ind++;
 
 		printArray(sut, "\nAdded 2 items");
 		Integer initial_size = sut.getItemArrayLength();
-		
-		sut.add(ind-1, indexMessagePrint(ind));
+
+		sut.add(ind - 1, indexMessagePrint(ind));
 		ind++;
 		printArray(sut, "\nAdded 1 items -> Array * 2");
 		Integer final_size = sut.getItemArrayLength();
-		
+
 		Integer expectedResult = final_size / initial_size;
-		
+
 		// Assert: 5th index should be number 5:
 		assertEquals(2, expectedResult);
 	}
+
 	@Test
 	void should_decrease_array_list_size_by_two() {
 		// Arrange:
@@ -364,7 +364,7 @@ class CustomArrayListTest {
 		ind++;
 
 		printArray(sut, "\nAdded 2 items");
-		
+
 		sut.add(indexMessagePrint(ind));
 		ind++;
 		Integer initial_size = sut.getItemArrayLength();
@@ -372,8 +372,7 @@ class CustomArrayListTest {
 
 		sut.remove(3);
 		printArray(sut, "\nRemoved 1 item -> Array / 2");
-		
-		
+
 		Integer final_size = sut.getItemArrayLength();
 		System.out.println("initial is: " + initial_size);
 		System.out.println(final_size);
@@ -382,11 +381,12 @@ class CustomArrayListTest {
 		// Assert: 5th index should be number 5:
 		assertEquals(2, expectedResult);
 	}
-	
-	// Helper Functions:
+
+	// Helper Functions for printing/debugging:
 	public static String indexMessagePrint(Integer ind) {
-		return "Item " + (ind-1);
+		return "Item " + (ind - 1);
 	}
+
 	public static void printArray(CustomArrayList<String> sut, String message) {
 		// Print out the array based on dynamic array current size:
 		System.out.println(message);
