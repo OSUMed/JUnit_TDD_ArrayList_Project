a0d78dc8c2aa78b67639ea3ae0c98727d02372f6
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-08-21 18:39:44 -0700

    replace copy method with Arrays.copyOf() ; add unit tests for return values

--- src/sri_assignment_7/com/coderscampus/sri/CustomArrayList.java
+++ src/sri_assignment_7/com/coderscampus/sri/CustomArrayList.java
@@ -1,5 +1,7 @@
 package sri_assignment_7.com.coderscampus.sri;
 
+import java.util.Arrays;
+
 public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
 	private Integer size = 0;
@@ -7,7 +9,8 @@ public class CustomArrayList<T> implements CustomList<T> {
 	@Override
 	public boolean add(T item) {
 		if (items.length == size) {
-			expandBackingObjectArray();
+			items = Arrays.copyOf(items, items.length * 2);
+//			expandBackingObjectArray();
 		}
 		
 		// For all calls, add to array and update tracking variables:
@@ -36,6 +39,7 @@ public class CustomArrayList<T> implements CustomList<T> {
 		return this.size;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public T get(int index) throws IndexOutOfBoundsException {
 		if (index > size) throw new IndexOutOfBoundsException();
@@ -49,11 +53,11 @@ public class CustomArrayList<T> implements CustomList<T> {
 	@Override
 	public boolean add(int index, T item) throws IndexOutOfBoundsException {
 		// Throw if new index is above size or not at new end of list:
-		if (index > size && index != size+1) throw new IndexOutOfBoundsException();
+		if (index > size+1) throw new IndexOutOfBoundsException();
 		
 		// check if array needs to inc size 2:
 		if (items.length == size) {
-			expandBackingObjectArray();
+			items = Arrays.copyOf(items, items.length * 2);
 		}
 		
 		
@@ -68,24 +72,36 @@ public class CustomArrayList<T> implements CustomList<T> {
 		return true;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public T remove(int index) throws IndexOutOfBoundsException {
 		// TODO Auto-generated method stub
 		if (index > size || index < 0) throw new IndexOutOfBoundsException();
-		
+		Integer removed_value = 0;
 		// from index, arr[i] = arr[i+1] until end of list
 		// stop at size-1 so we grab the last index with i+1 only
-		for (int i = index; i <= size-1; i++) {
+		// 0 1 2 3 4 5 6 7 8 9  10 11 12 13 14 15 16 17 18 19 20
+		// 0 1 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+		//           x
+		// i = 5 
+		
+		for (int i = index; i < size; i++) {
+			if (i == size) {
+				break;
+			}
+			if (i == index) {
+				removed_value = (Integer) items[i];
+			}
 			items[i] = items[i+1];
 		}
 		
 		// check if items.length() is half of size. If so, reduce 
 		// array by size 2
 		if (items.length/2 == size) {
-			reduceBackingObjectArray();
+			items = Arrays.copyOf(items, items.length / 2);
 		} 
 		
-		return null;
+		return (T)removed_value;
 	}
 
 }
--- test/sri_assignment_7/com/coderscampus/sri/CustomArrayListTest.java
+++ test/sri_assignment_7/com/coderscampus/sri/CustomArrayListTest.java
@@ -76,37 +76,6 @@ class CustomArrayListTest {
 		assertEquals(true, expectedResult);
 	}
 
-	// Check if getSize method works:
-	@Test
-	void should_return_size_1_with_add_1_item() {
-		// Arrange:
-		CustomList<Integer> sut = new CustomArrayList<>();
-
-		// Act
-		sut.add(10);
-		Integer expectedSize = sut.getSize();
-
-		// Assert: Integer is in the first index of customList
-		assertEquals(1, expectedSize);
-	}
-
-	// Check if get method works:
-	@Test
-	void should_return_correct_value_at_index_5() {
-		// Arrange:
-		CustomList<Integer> sut = new CustomArrayList<>();
-
-		// Act
-		Integer number = 0;
-		for (int i = 1; i < 10; i++) {
-			sut.add(number++);
-		}
-		Integer expectedResult = sut.get(5);
-
-		// Assert: 5th index should be number 5:
-		assertEquals(5, expectedResult);
-	}
-
 	// Next are to test if Exceptions are being triggered for the right cases:
 	@Test
 	void should_return_out_of_bounds_exception_for_get_index() {
@@ -158,6 +127,7 @@ class CustomArrayListTest {
 		
 		// Act
 		sut.add(10);
+
 		
 		// Assert: Integer is in the first index of customList
 		assertThrows(IndexOutOfBoundsException.class, () -> {
@@ -177,5 +147,150 @@ class CustomArrayListTest {
 			sut.remove(-3);
 		});
 	}
+	
+	// Check Returns
+	@Test
+	void should_return_actual_item_removed_from_list() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+		
+		// Act
+		sut.add(10);
+		Integer removed_value = sut.remove(0);
+		Integer size_after_removeal = sut.getSize();
+		
+		// Assert: Integer is in the first index of customList
+		assertEquals(0, size_after_removeal);
+		assertEquals(10, removed_value);
+	}
+	@Test
+	void should_return_true_when_item_added_with_index() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
 
+		// Act
+		Boolean result = sut.add(0,10);
+
+		// Assert: Integer is in the first index of customList
+		assertEquals(true, result);
+	}
+	@Test
+	void should_return_true_when_item_added_without_index() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+
+		// Act
+		Boolean result = sut.add(10);
+
+		// Assert: Integer is in the first index of customList
+		assertEquals(true, result);
+	}
+	@Test
+	void should_return_false_when_item__not_added_without_index() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+
+		// Act
+		Boolean result = sut.add(Integer.MAX_VALUE);
+
+		// overflow error(Max_value+1) should be false?
+		assertEquals(false, result);
+	}
+	// Check if getSize method works:
+	@Test
+	void should_return_size_1_with_add_1_item() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+
+		// Act
+		sut.add(10);
+		Integer expectedSize = sut.getSize();
+
+		// Assert: Integer is in the first index of customList
+		assertEquals(1, expectedSize);
+	}
+
+	// Check if get method works:
+	@Test
+	void should_return_correct_value_at_index_5() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+
+		// Act
+		Integer number = 0;
+		for (int i = 1; i < 10; i++) {
+			sut.add(number++);
+		}
+		Integer expectedResult = sut.get(5);
+
+		// Assert: 5th index should be number 5:
+		assertEquals(5, expectedResult);
+	}
+	
+	// Tests if length was increased by 2:
+	@Test
+	void should_increase_array_list_size_by_two() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+		
+		// Act
+		Integer number = 0;
+		for (int i = 1; i < 10; i++) {
+			sut.add(number++);
+		}
+		Integer initial_size = sut.getSize();
+		for (int i = 1; i < 10; i++) {
+			sut.add(number++);
+		}
+		Integer final_size = sut.getSize();
+		Integer expectedResult = final_size / initial_size;
+		
+		// Assert: 5th index should be number 5:
+		assertEquals(2, expectedResult);
+	}
+	@Test
+	void should_increase_array_list_size_by_two_for_add_with_index() {
+		// Arrange:
+		System.out.println("In error one");
+		CustomList<Integer> sut = new CustomArrayList<>();
+
+		// Act
+		Integer number = 0;
+		for (int i = 0; i <= 10; i++) {
+			sut.add(number, number);
+			number++;
+		}
+		Integer initial_size = sut.length();
+		for (int i = 0; i <= 10; i++) {
+			sut.add(number, number);
+			number++;
+		}
+		Integer final_size = sut.getSize();
+		System.out.println(initial_size);
+		System.out.println(final_size);
+		Integer expectedResult = final_size / initial_size;
+
+		// Assert: 5th index should be number 5:
+		assertEquals(2, expectedResult);
+	}
+	@Test
+	void should_decrease_array_list_size_by_two() {
+		// Arrange:
+		CustomList<Integer> sut = new CustomArrayList<>();
+
+		// Act
+		Integer number = 0;
+		for (int i = 0; i < 21; i++) {
+			sut.add(number++);
+		}
+		Integer initial_size = sut.;
+		sut.remove(3);
+		Integer final_size = sut.getSize();
+		System.out.println("initial is: " + initial_size);
+		System.out.println(final_size);
+		Integer expectedResult = initial_size / final_size;
+
+		// Assert: 5th index should be number 5:
+		assertEquals(2, expectedResult);
+	}
 }
